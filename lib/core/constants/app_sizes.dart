106a0c7c17a011398c5e305c4e8042a4aac5dc93
Nguyễn Huỳnh Thành Danh <thanhdanh130425@gmail.com>
2026-04-23 10:22:07 +0700

    create folder constants

---
+++ lib/core/constants/app_sizes.dart
@@ -0,0 +1,20 @@
+class AppSizes {
+  static const double xs = 4.0;
+  static const double sm = 8.0;
+  static const double md = 16.0;
+  static const double lg = 24.0;
+  static const double xl = 32.0;
+  static const double xxl = 40.0;
+  static const double xxxl = 48.0;
+  
+  static const double avatarSize = 48.0;
+  
+  static const double radiusSm = 4.0;
+  static const double radiusMd = 8.0;
+  static const double radiusLg = 16.0;
+  static const double radiusXl = 24.0;
+  
+  static const double iconMd = 24.0;
+  
+  static const double sidebarWidth = 280.0;
+}
