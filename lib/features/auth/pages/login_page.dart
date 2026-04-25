25a5091bcd4136a1342f81d8d01a1c25266a7c06
Nguyễn Huỳnh Thành Danh <thanhdanh130425@gmail.com>
2026-04-25 21:09:49 +0700

    create build_logo

--- lib/features/auth/pages/login_page.dart
+++ lib/features/auth/pages/login_page.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:go_router/go_router.dart';
-import '../../../../core/constants/app_colors.dart';
-import '../../../../core/constants/app_sizes.dart';
-import '../../../../core/utils/extensions.dart';
+import '../../../core/constants/app_colors.dart';
+import '../../../core/constants/app_sizes.dart';
+import '../../../core/utils/extensions.dart';
 import '../providers/auth_provider.dart';
+import '../widgets/build_logo.dart';
 import '../widgets/build_label.dart';
 
 /// LoginPage — Trang đăng nhập
@@ -66,7 +67,7 @@ class _LoginPageState extends State<LoginPage> {
                 SizedBox(height: screenHeight * 0.08),
 
                 // ─── Logo & Title ───
-                _buildLogo(isDark),
+                buildLogo(isDark),
                 SizedBox(height: screenHeight * 0.06),
 
                 // ─── Welcome Text ───
@@ -209,44 +210,4 @@ class _LoginPageState extends State<LoginPage> {
       ),
     );
   }
-
-  Widget _buildLogo(bool isDark) {
-    return Row(
-      children: [
-        Container(
-          width: 44,
-          height: 44,
-          decoration: BoxDecoration(
-            gradient: const LinearGradient(
-              colors: [AppColors.primary, AppColors.primaryLight],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-            borderRadius: BorderRadius.circular(AppSizes.radiusMd),
-          ),
-          child: const Icon(Icons.check_rounded, color: Colors.white, size: 28),
-        ),
-        const SizedBox(width: AppSizes.md),
-        Text(
-          'To-Do List',
-          style: TextStyle(
-            fontSize: 22,
-            fontWeight: FontWeight.w700,
-            color: isDark ? AppColors.textPrimaryDark : AppColors.textPrimary,
-          ),
-        ),
-      ],
-    );
-  }
-
-  // Widget _buildLabel(String text, bool isDark) {
-  //   return Text(
-  //     text,
-  //     style: TextStyle(
-  //       fontSize: 13,
-  //       fontWeight: FontWeight.w600,
-  //       color: isDark ? AppColors.textPrimaryDark : AppColors.textPrimary,
-  //     ),
-  //   );
-  // }
 }
